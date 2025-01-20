b671affd54be459b899e436a16347fd0101cdc8a
Mohammad Samory <Mohammad.Samoury@optimumpartners.co>
2025-01-20 13:10:23 +0300

    --

---
+++ library_management/domain/Book/entity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from uuid import UUID
+from datetime import date
+from typing import Any
+
+
+from library_management.domain.shared.base_entity import Base
+
+
+# @dataclass
---
+++ library_management/infrastructure/database/engine.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Database Configuration (SQLAlchemy)
+DATABASE_URL =  "postgresql+psycopg2://mohasam:pass@db:5432/my_database"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
